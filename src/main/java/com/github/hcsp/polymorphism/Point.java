9ee2b5f1a245f734468efba86402138655cf4dd1
bonnie1115 <49628449+bonnie1115@users.noreply.github.com>
2020-02-11 14:17:24 +0800

    多重字段排序 (#112)
    多重字段排序 (#112)

--- src/main/java/com/github/hcsp/polymorphism/Point.java
+++ src/main/java/com/github/hcsp/polymorphism/Point.java
@@ -1,10 +1,9 @@
 package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
-public class Point {
+public class Point implements Comparable<Point> {
 
     private final int x;
     private final int y;
@@ -53,7 +52,12 @@ public class Point {
 
     // 按照先x再y，从小到大的顺序排序
     // 例如排序后的结果应该是 (-1, 1) (1, -1) (2, -1) (2, 0) (2, 1)
-    public static List<Point> sort(List<Point> points) {}
+    //自然排序  a<b -1 a>b 1 a=b 0
+    public static List<Point> sort(List<Point> points) {
+        Collections.sort(points);
+        return points;
+
+    }
 
     public static void main(String[] args) throws IOException {
         List<Point> points =
@@ -64,5 +68,25 @@ public class Point {
                         new Point(2, 1),
                         new Point(2, -1));
         System.out.println(Point.sort(points));
+    }
+
+
+
+
+    @Override
+    public int compareTo(Point that) {
+        System.out.println("thisx:"+this.x);
+        System.out.println("thatx:"+that.x);
+        if(this.x<that.x){   //this.x?   that.x?
+            return -1;
+        }else if(this.x>that.x){
+            return 1;
+        }
+        if(this.y<that.y){
+            return -1;
+        }else if(this.y>that.y){
+            return 1;
+        }
+        return  0;
     }
 }
