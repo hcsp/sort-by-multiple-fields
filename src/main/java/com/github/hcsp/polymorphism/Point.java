affff213dad3aafa487154ff74056c0da0deba34
2014 <suhanma@gmail.com>
2021-01-25 17:27:55 -0800

    sort (#197)
    sort (#197)

--- src/main/java/com/github/hcsp/polymorphism/Point.java
+++ src/main/java/com/github/hcsp/polymorphism/Point.java
@@ -1,8 +1,7 @@
 package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Point {
 
@@ -53,7 +52,16 @@ public class Point {
 
     // 按照先x再y，从小到大的顺序排序
     // 例如排序后的结果应该是 (-1, 1) (1, -1) (2, -1) (2, 0) (2, 1)
-    public static List<Point> sort(List<Point> points) {}
+    public static List<Point> sort(List<Point> points) {
+        Comparator<Point> comp = (Point a, Point b) -> {
+          int result = Integer.compare(a.getX(), b.getX());
+          if (result == 0) return  Integer.compare(a.getY(), b.getY());
+          return result;
+        };
+
+        points.sort(comp);
+        return points;
+    }
 
     public static void main(String[] args) throws IOException {
         List<Point> points =
